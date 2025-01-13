ee0cb26c4ca445c9439c181bf25b722a389475ad
jlarry77 <justynlarry@gmail.com>
2025-01-13 09:04:45 -0700

    no changes made

--- login_template/index.php
+++ login_template/index.php
@@ -7,6 +7,32 @@
     <title>Login Template</title>
 </head>
 <body>
+    <!-- Header Section -->
+    <header>
+
+        <!-- Navigation Bar Section -->
+        <nav>
+
+        </nav>
+    </header>
     
+    <!-- Form to submit user name and password -->
+
+    
+    <form class="login_form" action="submit" method="POST">
+        <h3>User Login</h3>
+    
+        <!-- Inputs for Form -->
+        <div class="user">
+            <h3>User Name <i>required</i></h3>
+            <input type="text" namne="user_name" placeholder="User Name">
+        </div>
+        
+        <div class="password">
+            <h3>Password <i>required</i></h3>
+            <input type="password" name="password" placeholder="Password">
+        </div>
+        </form>
+
 </body>
 </html>
