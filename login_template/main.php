f1622d418225bd91fd8a84623582fd21bfd1c1ba
jlarry77 <justynlarry@gmail.com>
2025-01-13 12:06:48 -0700

    Fix syntax and spelling errors in:
    config_session.inc.php
    login_contr.inc.php
    login_model.inc.php
    login.inc.php
    index.php
    
    and create landing page main.php for the next phase
    of the operation.  At this point though,
    (outside of styling )the template is complete,
    and anything further will be added on when
    the need arises for the template.
    
    next phase is to mess around with some cool
    .css ideas I found this weekend :)

---
+++ login_template/main.php
@@ -0,0 +1,23 @@
+<?php
+// Error Handlers
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Include necessary files
+require_once 'includes/dbh.inc.php';
+// require_once 'includes/fetch_insp_ids.inc.php'; // Fetch Inspector IDs
+require_once 'includes/login_view.inc.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Main.php</h3>
+</body>
+</html>
