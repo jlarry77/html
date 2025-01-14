8089333dbf2538f1a32e743bf64940217a4fda8d
jlarry77 <justynlarry@gmail.com>
2025-01-14 11:04:46 -0700

    Updated dir_contents to include a new sub-folder, add subfolder /playground/ and it's accompanying style-sheet.

---
+++ playground/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
+
+    <title>Document</title>
+</head>
+<body>
+<img src="/images/digital-sphere.jpg" alt="Digital Sphere">
+    <header class = "header">
+        <div class="title">
+            <h1 style="color:red;">Online</h1><h1> Digital Playground</h1>
+        </div>
+        <div class = "head_links">
+
+    </div>
+    </header>
+
+</body>
+</html>
