ca4d02f5afa9ba6811b72b58830010c2a68aab06
jlarry77 <justynlarry@gmail.com>
2025-01-22 11:23:47 -0700

    Remove references to email field in signup_contr.inc.php.

--- blog/includes/signup_contr.inc.php
+++ blog/includes/signup_contr.inc.php
@@ -4,8 +4,8 @@
 
 declare(strict_types=1); 
 
-function is_input_empty(string $username, string $pwd, string $email) {
-    if (empty($username) || empty($pwd) || empty($email)) {
+function is_input_empty(string $username, string $pwd) {
+    if (empty($username) || empty($pwd)) {
         return true;
     }
     else {
@@ -13,14 +13,6 @@ function is_input_empty(string $username, string $pwd, string $email) {
     }
 }
 
-function is_email_invalid(string $email) {
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
 
 function is_username_taken(object $pdo, string $username) {
     if (get_username($pdo, $username)) {
@@ -31,15 +23,8 @@ function is_username_taken(object $pdo, string $username) {
     }
 }
 
-function is_email_registered(object $pdo, string $email) {
-    if (get_email($pdo, $email)) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
 
-function create_user(object $pdo, string $pwd, string $username, string $email) {
-   set_user($pdo, $pwd, $username, $email);
+
+function create_user(object $pdo, string $pwd, string $username) {
+   set_user($pdo, $pwd, $username);
 }
