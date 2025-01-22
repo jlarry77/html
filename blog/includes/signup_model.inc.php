99ee7173234415c7c1824a3378bacf2ad99d9e30
jlarry77 <justynlarry@gmail.com>
2025-01-22 11:42:54 -0700

    Create signup_model.inc.php to hash passwords created by user, and input username and hashed password into database.

---
+++ blog/includes/signup_model.inc.php
@@ -0,0 +1,33 @@
+<?php
+
+//  Model Form: Takes care of querying the Database
+
+declare(strict_types=1);    // Set to true, allows type declarations
+
+function get_username(object $pdo, string $username) {
+
+    $query = "SELECT user_name FROM users WHERE user_name = :user_name;";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(":user_name", $username);
+    $stmt->execute();
+
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $result;
+
+}
+
+
+function set_user(object $pdo, string $username, string $pwd) {
+    $query = "INSERT INTO users (user_name, pwd) VALUES (:user_name, :pwd);";
+    $stmt = $pdo->prepare($query);
+    
+    $options = [
+        'cost' => 12
+    ];
+
+    $hashedPwd = password_hash($pwd, PASSWORD_BCRYPT, $options);
+    
+    $stmt->bindParam(":username", $username);
+    $stmt->bindParam(":pwd", $hashedPwd);
+    $stmt->execute();
+}
