f717a32404127f09868769cd4708cdc0f4820171
jlarry77 <justynlarry@gmail.com>
2025-01-22 09:44:19 -0700

    Added log in fields to login.php, and a link back to the home page (index.php).

--- blog/login.php
+++ blog/login.php
@@ -7,6 +7,41 @@
     <title>Blog Login Page</title>
 </head>
 <body>
+        <!-- Header and Navigation Bar-->
+        <header class="head-section">
+        <h1 class="title-section">Blog Title (TBD)</h1>
+        <nav class="nav-section">
+            <a href="index.php">Home</a>
+            <form action="includes/logout.inc.php">
+                <button class="admin-logout" type="submit">Log Out</button>
+            </form>
+        </nav>
+    </header>
     
+
+    <!-- Form to submit user name and password -->
+
+    <div class="login-container">
+        <form class="login_form" action="includes/login.inc.php" method="POST">
+            <h3>User Login</h3>
+        
+            <!-- Inputs for Form -->
+            <div class="user">
+                <h3>User Name <i>required</i></h3>
+                <input type="text" name="user_name" placeholder="User Name">
+            </div>
+            
+            <div class="password">
+                <h3>Password <i>required</i></h3>
+                <input type="password" name="user_pw" placeholder="Password">
+            </div>
+
+            <div class="submit-button">
+                <button type="submit">Submit</button>
+            </div>
+        </form>
+    </div>
+
+
 </body>
 </html>
