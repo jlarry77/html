2b86a7404421c9ab17a869188a18af497dc0141e
jlarry77 <justynlarry@gmail.com>
2025-01-22 11:07:40 -0700

    Added form, input, and button for blog post referencing /includes/blog.inc.php.

--- blog/admin.php
+++ blog/admin.php
@@ -7,17 +7,29 @@
     <title>Blog Admin/Post Entry</title>
 </head>
 <body>
-        <!-- Registration Section -->
-        <div class="index-registration">
-          <h3>User Registration</h3>
-            <form action="includes/signup.inc.php" method="POST">
-                <input type="text" name="username" placeholder="User Name">
-                <input type="password" name="pwd" placeholder="Password">
-                <input type="text" name="email" placeholder="e-mail">
-                <button type="submit">Add User</button> 
-            </form>
-            <?php check_signup_errors(); ?>
-        </div>
+
+    <!-- Blog Input Section -->
+     <div class="blog-input">
+        <h3>Blog Content Creation</h3>
+        <form action="includes/blog.inc.php" method="POST">
+            <input type="text" name="title" placeholder="Post Title">
+            <input type="text" name="blog-content" placeholder="Blog Content">
+            <button type="submit">Submit Post</button>
+        </form>
+
+     </div>
+
+    <!-- Registration Section -->
+    <div class="index-registration">
+        <h3>User Registration</h3>
+        <form action="includes/signup.inc.php" method="POST">
+            <input type="text" name="username" placeholder="User Name">
+            <input type="password" name="pwd" placeholder="Password">
+            <input type="text" name="email" placeholder="e-mail">
+            <button type="submit">Add User</button> 
+        </form>
+        <?php check_signup_errors(); ?>
+    </div>
 
 
 
