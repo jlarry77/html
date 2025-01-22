3fa3613a8717a9c3c41c05d2e89674609dbb8043
jlarry77 <justynlarry@gmail.com>
2025-01-22 10:52:04 -0700

    Added form, inputs, and submit button to create a user on the admin.php page.

--- blog/admin.php
+++ blog/admin.php
@@ -7,6 +7,19 @@
     <title>Blog Admin/Post Entry</title>
 </head>
 <body>
-    
+        <!-- Registration Section -->
+        <div class="index-registration">
+          <h3>User Registration</h3>
+            <form action="includes/signup.inc.php" method="POST">
+                <input type="text" name="username" placeholder="User Name">
+                <input type="password" name="pwd" placeholder="Password">
+                <input type="text" name="email" placeholder="e-mail">
+                <button type="submit">Add User</button> 
+            </form>
+            <?php check_signup_errors(); ?>
+        </div>
+
+
+
 </body>
 </html>
