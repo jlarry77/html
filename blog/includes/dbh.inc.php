dfd57b0ddcb30b84831b2864da94cd44d1d5c898
jlarry77 <justynlarry@gmail.com>
2025-01-22 09:34:54 -0700

    Created Database Handler/Connection file, still need to create MySQL Database, and name, so DB name is currently listed as 'TBD'.

--- blog/includes/dbh.inc.php
+++ blog/includes/dbh.inc.php
@@ -1,1 +1,17 @@
 <?php
+
+
+//  Database Connection File //
+
+$dsn = "mysql:host=100.107.139.13; dbname= TBD ";
+
+$dbusername = "root";
+$dbpassword = "Anchor77Crown";
+
+try {
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    echo "Connection Failed: " . $e->getMessage();
+}
