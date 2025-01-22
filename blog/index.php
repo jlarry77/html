98ce2789d64dabb3a49a927cb700db4cd700a97e
jlarry77 <justynlarry@gmail.com>
2025-01-22 09:14:33 -0700

    Created basic framework for the index.php, and created files and foldersthat will be required intially to start creating the blog-post page\  (all listed below).

---
+++ blog/index.php
@@ -0,0 +1,37 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/styles.css">
+    <title>JML Programming Blog</title>
+</head>
+<body>
+
+    <!-- Header and Navigation Bar-->
+    <header class="head-section">
+        <h1 class="title-section">Blog Title (TBD)</h1>
+        <nav class="nav-section">
+            <button class="admin-login" type="submit">Login</button>
+            <button class="admin-logout" type="submit">Log Out</button>
+        </nav>
+    </header>
+
+    <!-- Main Content / Blog Posts -->
+
+    <div class="main-content">
+        <?php ?>
+    </div>
+    
+
+    <!-- Footer Section -->
+     <footer class="footer">
+
+
+     </footer>
+</body>
+</html>
